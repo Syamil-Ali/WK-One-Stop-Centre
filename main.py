4a19bdbe1ca8bbd5c5b3abcae253aa2c311fac3b
Syamil Ali <119921673+Syamil-Ali@users.noreply.github.com>
2025-06-21 00:48:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import layout.header as header
 # import page
 from mql_automation import main as mam
 from opportunity_generator import main as ogm
+from pdf_to_table import main as ptt
+from ultimate_parent_generator import main as upg
 
 
 
+from starlette.formparsers import MultiPartParser
 MultiPartParser.spool_max_size = 1024 * 1024 * 512  # 512 MB
 
 
@@ -45,7 +48,7 @@ async def main_app():
     # Render Process MQL
 
     #### UI BODY ###
-    with ui.element('div').style('height: 80vh;').classes('flex pt-[20vh] pb-[2rem] justify-center max-w-[800px] w-full mx-auto'):
+    with ui.element('div').style('height: 90vh;').classes('flex pt-[10vh] justify-center max-w-[800px] w-full mx-auto'):
         with ui.column().classes('items-center w-full gap-4'):
 
             # Title
@@ -55,7 +58,7 @@ async def main_app():
 
 
             with ui.row().classes('flex-wrap gap-4 justify-center mt-6'):
-                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] items-center justify-center'):
+                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] h-[300px] items-center justify-center'):
                     # "1" styled as an icon
                     ui.label('1').classes(
                         'number-circle font-poppins'
@@ -73,7 +76,7 @@ async def main_app():
                     ).props('outline')
 
 
-                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] items-center justify-center'):
+                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] h-[300px] items-center justify-center'):
                     # "1" styled as an icon
                     ui.label('2').classes(
                         'number-circle font-poppins'
@@ -89,12 +92,43 @@ async def main_app():
                     ).classes(
                         'mt-auto self-start text-black py-1 font-poppins normal-case border border-black rounded-md bg-orange'
                     ).props('outline')
-                    
-                
-                
-                    
-                
 
+
+                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] h-[300px] items-center justify-center'):
+                    # "1" styled as an icon
+                    ui.label('3').classes(
+                        'number-circle font-poppins'
+                    )
+                    ui.label('PDF To Table').classes('text-lg font-semibold font-poppins text-black mt-2 px-4 py-2 w-full text-center')
+                    ui.label('Convert PDF Data to Table Format'
+                    ).classes(
+                        'text-sm font-poppins text-left mb-4 w-full border-t border-black pt-5'
+                    )
+                    ui.button(
+                        'Go to Page',
+                        on_click=lambda: ui.navigate.to('/pdf_to_table'),
+                    ).classes(
+                        'mt-auto self-start text-black py-1 font-poppins normal-case border border-black rounded-md bg-orange'
+                    ).props('outline')
+
+                
+                with ui.card().tight().classes('bg-[#9400FF] text-white p-4 rounded-lg shadow-md w-[250px] h-[300px] items-center justify-center'):
+                    # "1" styled as an icon
+                    ui.label('4').classes(
+                        'number-circle font-poppins'
+                    )
+                    ui.label('Ultimate Parent Generator').classes('text-lg font-semibold font-poppins text-black mt-2 px-4 py-2 w-full text-center')
+                    ui.label('Generate Ultimate Parent Info from Salesforce using CE-Number'
+                    ).classes(
+                        'text-sm font-poppins text-left mb-4 w-full border-t border-black pt-5'
+                    )
+                    ui.button(
+                        'Go to Page',
+                        on_click=lambda: ui.navigate.to('/ultimate_parent_generator'),
+                    ).classes(
+                        'mt-auto self-start text-black py-1 font-poppins normal-case border border-black rounded-md bg-orange'
+                    ).props('outline')
+                    
 
 
 # Local deploymnet
@@ -105,5 +139,6 @@ ui.run(
     host='0.0.0.0',
     port=int(os.getenv('PORT', 8080)),  # use Railway's port, fallback to 8080 locally
     title='WK One Stop Centre',
-    reload=False
+    reload=False,
+    reconnect_timeout=300
 )
